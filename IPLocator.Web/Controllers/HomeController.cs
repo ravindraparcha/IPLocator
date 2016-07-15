ba051e32e6116275ab47d903e7db0f5d8cdca806
Ravindra Parcha <ravindraparcha@outlook.com>
2016-07-15 20:38:37 +0530

    Search near by places by ip address

--- IPLocator.Web/Controllers/HomeController.cs
+++ IPLocator.Web/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace IPLocator.Web.Controllers
     public class HomeController : Controller
     {
         private static  string dbConnString = ConfigurationManager.ConnectionStrings["DBConnString"].ToString();
+        IPDBClass ipDBClass = new IPDBClass(dbConnString);
         public ActionResult Index()
         {
             return View("Index", new PredefinedPlaceInfo());
@@ -20,12 +21,24 @@ namespace IPLocator.Web.Controllers
 
         public PartialViewResult GetIPDetails(string IPAddress)
         {
-            IPDBClass ipDBClass = new IPDBClass(dbConnString);            
+               
             return PartialView("~/Views/Home/_IPDetail.cshtml", ipDBClass.GetIPAddressDetails(IPAddress));
         }
 
-        public PartialViewResult GetNearbyPlaces(string lat, string longi, string place, string radius)
+        public PartialViewResult GetNearbyPlaces(string lat, string longi, string place, string radius,string ipAddress)
         {
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                IPDetails ipDetails = ipDBClass.GetIPAddressDetails(ipAddress);
+                lat = ipDetails.Latitude;
+                longi = ipDetails.Longitude;
+                ViewBag.InfoMsg = string.Format("Near by places for \"{0}\"- ",ipAddress);
+            }
+            else
+            {
+                ViewBag.InfoMsg = "Near by places from current location- ";
+            }
+
             //http://stackoverflow.com/questions/26694049/how-to-use-google-maps-simple-api-on-localhost
             // link for google maps in mvc5 http://www.c-sharpcorner.com/article/integrating-google-maps-places-and-geocode-apis-with-asp-net-mvc-5/
             //create the key of type- Google Places API Web Service
@@ -34,15 +47,17 @@ namespace IPLocator.Web.Controllers
             //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=YOUR_API_KEY
             //string placeApiUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&key={1}";
 
-            string placeApiUrl = ConfigurationManager.AppSettings["GoogleApiUrl"];
+            string placeApiUrl = ConfigurationManager.AppSettings["GoogleApiUrl"].Replace("@:", "&");
             List<PlacesInfo> placesInfo = new List<PlacesInfo>();
            
-            placeApiUrl = placeApiUrl.Replace("@:", "&");
-            placeApiUrl = placeApiUrl.Replace("{0}", lat);
-            placeApiUrl = placeApiUrl.Replace("{1}", longi);
-            placeApiUrl = placeApiUrl.Replace("{2}", radius);
-            placeApiUrl = placeApiUrl.Replace("{3}", place);
-            placeApiUrl = placeApiUrl.Replace("{4}", ConfigurationManager.AppSettings["GoogleAPIServerKey"]);
+            //placeApiUrl = placeApiUrl.Replace("@:", "&");
+            //placeApiUrl = placeApiUrl.Replace("{0}", lat);
+            //placeApiUrl = placeApiUrl.Replace("{1}", longi);
+            //placeApiUrl = placeApiUrl.Replace("{2}", radius);
+            //placeApiUrl = placeApiUrl.Replace("{3}", place);
+            //placeApiUrl = placeApiUrl.Replace("{4}", ConfigurationManager.AppSettings["GoogleAPIServerKey"]);
+
+            placeApiUrl = string.Format(placeApiUrl, lat, longi, radius, place, ConfigurationManager.AppSettings["GoogleAPIServerKey"]);
 
             var result = new System.Net.WebClient().DownloadString(placeApiUrl);
             placesInfo = JsonConvert.DeserializeObject<Places>(result).results;
--- IPLocator.Web/Scripts/main.js
+++ IPLocator.Web/Scripts/main.js
@@ -1,96 +1,101 @@
-﻿function validateIPAddress(e) { return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(e) ? !0 : !1 } function getGeoCoordinates() { function e(e) { latitude = e.coords.latitude, long = e.coords.longitude } function a() { alert("Unable to retrieve your location") } return navigator.geolocation ? void navigator.geolocation.getCurrentPosition(e, a) : void alert("<p>Geolocation is not supported by your browser</p>") } function getIPDetails() { var e = $("#txtIPAddress").val().trim(); return validateIPAddress(e) ? ($("#divIPData").css("color", "black"), addRemoveBusyImage("divIPData", !0), void $.ajax({ type: "GET", url: GetIPDetailsUrl, data: { IPAddress: e }, dataType: "html", success: function (e) { addRemoveBusyImage("divIPData", !1), $("#divIPData").html(e) }, error: function (e) { addRemoveBusyImage("divIPData", !1), $("#divIPData").html("Error occurred! Cannot load the content") } })) : ($("#divIPData").html("Invalid IP Address. Please check again!").css("color", "red"), !1) } function addRemoveBusyImage(e, a) { $("#" + e).empty(), a ? $("#" + e).prepend($("<img>", { id: "busyImg", src: "/Content/Images/loadingAnimation.gif", height: "100" })) : $("#" + e).removeClass("text-center") } function getNearbyPlaces() { addRemoveBusyImage("divPlaces", !0), $.ajax({ type: "GET", url: GetNearbyPlacesUrl, data: { lat: latitude, longi: long, place: $("#PlaceKey").val(), radius: $("#RadiusKey").val() }, dataType: "html", success: function (e) { addRemoveBusyImage("divPlaces", !1), $("#divPlaces").html(e) }, error: function (e) { addRemoveBusyImage("", !1), $("#divPlaces").html("Error occurred! Cannot load the content") } }) } $(function () { getGeoCoordinates() }); var latitude = "0", long = "0";
+﻿$(function () {
+    //$('.partialContent').each(function (index, item) {
+    //    var url = $(item).data('url');
+    //    if (url && url.length > 0) {
+    //        $(item).load(url);
+    //    }
+    //});
 
-//$(function () {
-//    //$('.partialContent').each(function (index, item) {
-//    //    var url = $(item).data('url');
-//    //    if (url && url.length > 0) {
-//    //        $(item).load(url);
-//    //    }
-//    //});
+    getGeoCoordinates();
 
-//    getGeoCoordinates();
+});
 
-//});
+function validateIPAddress(inputText) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(inputText)) {
+        return (true)
+    }
+    return false;
+}
+var latitude = "0";
+var long = "0";
+function getGeoCoordinates() {
+    if (!navigator.geolocation) {
+        alert("<p>Geolocation is not supported by your browser</p>");
+        return;
+    }
 
-//function validateIPAddress(inputText) {
-//    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(inputText)) {
-//        return (true)
-//    }
-//    return false;
-//}
-//var latitude = "0";
-//var long = "0";
-//function getGeoCoordinates() {
-//    if (!navigator.geolocation) {
-//        alert("<p>Geolocation is not supported by your browser</p>");
-//        return;
-//    }
+    function success(position) {
+        latitude = position.coords.latitude;
+        long = position.coords.longitude;
+    };
 
-//    function success(position) {
-//        latitude = position.coords.latitude;
-//        long = position.coords.longitude;
-//    };
-
-//    function error() {
-//        alert("Unable to retrieve your location");
-//    };
-//    navigator.geolocation.getCurrentPosition(success, error);
-//}
+    function error() {
+        alert("Unable to retrieve your location");
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+}
 
 
-//function getIPDetails() {
-//    var ip = $("#txtIPAddress").val().trim();
-//    if (!validateIPAddress(ip)) {
-//        $('#divIPData').html('Invalid IP Address. Please check again!').css('color','red');
-//        return false;
-//    }
-//    $('#divIPData').css('color', 'black');
-//    addRemoveBusyImage('divIPData', true);
-//    $.ajax({
-//        type: "GET",
-//        url: GetIPDetailsUrl,
-//        data: { IPAddress: ip },
-//        dataType: "html",
-//        success: function (data) {
-//            addRemoveBusyImage('divIPData', false);
-//            $('#divIPData').html(data);
+function getIPDetails() {
+    var ip = $("#txtIPAddress").val().trim();
+    if (!validateIPAddress(ip)) {
+        $('#divIPData').html('Invalid IP Address. Please check again!').css('color','red');
+        return false;
+    }
+    $('#divIPData').css('color', 'black');
+    addRemoveBusyImage('divIPData', true);
+    $.ajax({
+        type: "GET",
+        url: GetIPDetailsUrl,
+        data: { IPAddress: ip },
+        dataType: "html",
+        success: function (data) {
+            addRemoveBusyImage('divIPData', false);
+            $('#divIPData').html(data);
 
-//        },
-//        error: function (data) {
-//            addRemoveBusyImage('divIPData', false);
-//            $('#divIPData').html('Error occurred! Cannot load the content');
-//        }
-//    });
+        },
+        error: function (data) {
+            addRemoveBusyImage('divIPData', false);
+            $('#divIPData').html('Error occurred! Cannot load the content');
+        }
+    });
 
-//}
+}
 
-//function addRemoveBusyImage(divElement, isAdd) {
-//    $('#' + divElement).empty();
-//    if (isAdd) {
-//        // $('#' + divElement).addClass('text-center');
-//        $('#' + divElement).prepend($('<img>', { id: 'busyImg', src: '/Content/Images/loadingAnimation.gif', height: '100' }));
-//    }
-//    else {
-//        $('#' + divElement).removeClass('text-center');
-//    }
-//}
+function addRemoveBusyImage(divElement, isAdd) {
+    $('#' + divElement).empty();
+    if (isAdd) {
+        // $('#' + divElement).addClass('text-center');
+        $('#' + divElement).prepend($('<img>', { id: 'busyImg', src: '/Content/Images/loadingAnimation.gif', height: '100' }));
+    }
+    else {
+        $('#' + divElement).removeClass('text-center');
+    }
+}
 
-//function getNearbyPlaces() {
-//    debugger;
-//    addRemoveBusyImage('divPlaces', true);
-//    $.ajax({
-//        type: "GET",
-//        url: GetNearbyPlacesUrl,
-//        data: { lat: latitude, longi: long, place: $('#PlaceKey').val(), radius: $('#RadiusKey').val() },
-//        dataType: "html",
-//        success: function (data) {
-//            addRemoveBusyImage('divPlaces', false);
-//            $('#divPlaces').html(data);
+function getNearbyPlaces() {
+    var ip = $("#txtIPAddress").val().trim();
+    if (ip != "") {
+        if (!validateIPAddress(ip)) {
+            $('#divPlaces').html('Invalid IP Address. Please check again!').css('color', 'red');
+            return false;
+        }
+    }
+    $('#divPlaces').css('color', 'black');
+    addRemoveBusyImage('divPlaces', true);
+    $.ajax({
+        type: "GET",
+        url: GetNearbyPlacesUrl,
+        data: { lat: latitude, longi: long, place: $('#PlaceKey').val(), radius: $('#RadiusKey').val(), ipAddress: ip },
+        dataType: "html",
+        success: function (data) {
+            addRemoveBusyImage('divPlaces', false);
+            $('#divPlaces').html(data);
 
-//        },
-//        error: function (data) {
-//            addRemoveBusyImage('', false);
-//            $('#divPlaces').html('Error occurred! Cannot load the content');
-//        }
-//    });
-//}
+        },
+        error: function (data) {
+            addRemoveBusyImage('', false);
+            $('#divPlaces').html('Error occurred! Cannot load the content');
+        }
+    });
+}
